93271e3b95f4fb2593eac0d16a6ff8655e8d0920
Gildemberg Leite <gildembergleite@gmail.com>
2024-03-24 12:42:17 -0300

    feat: create avatar component

---
+++ packages/react/src/components/avatar.tsx
@@ -0,0 +1,48 @@
+import * as AvatarComponent from '@radix-ui/react-avatar'
+import { User } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+const AvatarContainer = styled(AvatarComponent.Root, {
+  borderRadius: '$full',
+  display: 'inline-block',
+  width: '$12',
+  height: '$12',
+  overflow: 'hidden',
+})
+
+const AvatarImage = styled(AvatarComponent.Image, {
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+  borderRadius: 'inherit',
+})
+
+const AvatarFallback = styled(AvatarComponent.Fallback, {
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 'inherit',
+  backgroundColor: '$gray-600',
+  color: '$gray-800',
+
+  svg: {
+    width: '$6',
+    height: '$6',
+  },
+})
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
