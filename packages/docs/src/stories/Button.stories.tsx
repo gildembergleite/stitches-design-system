bd41fdf393fae2b15a831184250d967dabf0d3e3
Gildemberg Leite <gildembergleite@gmail.com>
2024-03-24 08:15:44 -0300

    feat: create button story

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,22 @@
+import { Button, ButtonProps } from '@gildembergleite/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    primary: true,
+    label: 'Button',
+    children: 'Button'
+  },
+  
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Large: StoryObj<ButtonProps> = {
+  args: {
+    size: 'large',
+  }
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,17 +1,33 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
-  backgroundColor: '$system-500',
-  borderRadius: '$md',
+export const Button = styled('button', {
+  backgroundColor: '$system-300',
+  borderRadius: '$sm',
   fontFamily: '$default',
+  fontWeight: 'bold',
+  color: '$white',
+  border: 0,
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      medium: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+      large: {
+        fontSize: 18,
+        padding: '$4 $8',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'medium',
+  },
 })
 
-export function App() {
-  return (
-    <main>
-      <section>
-        <Button>Button</Button>
-      </section>
-    </main>
-  )
-}
+export type ButtonProps = ComponentProps<typeof Button>
