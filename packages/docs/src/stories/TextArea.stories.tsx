1ee4804da894e0225f2f45d5b9008575b3d61ab8
Gildemberg Leite <gildembergleite@gmail.com>
2024-03-25 09:11:01 -0300

    feat: create text area component and sotry

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,29 @@
+import { Box, Text, TextArea, TextAreaProps } from '@gildembergleite/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+
+export default {
+  title: 'Typography/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (Story) => (
+      <Box as='label' css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}>
+        <Text size='sm'>Message</Text>
+        {Story()}
+      </Box>
+    )
+  ]
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Enter a value',
+  }
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true
+  }
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -4,5 +4,6 @@ export * from './components/box'
 export * from './components/button'
 export * from './components/heading'
 export * from './components/text'
+export * from './components/text-area'
 export * from './components/text-input'
 
