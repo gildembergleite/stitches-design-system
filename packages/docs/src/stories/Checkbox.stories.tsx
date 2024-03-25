d2b114fece598f42de2d1c3668ef1b0a9b9453c5
Gildemberg Leite <gildembergleite@gmail.com>
2024-03-25 09:47:17 -0300

    feat: create checkbox component and story

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,24 @@
+import { Box, Checkbox, CheckboxProps, Text } from '@gildembergleite/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+
+export default {
+  title: 'Typography/Checkbox',
+  component: Checkbox,
+  decorators: [
+    (Story) => (
+      <Box as='label' css={{ display: 'flex', gap: '$2' }}>
+        {Story()}
+        <Text size='sm'>Accept terms of use</Text>
+      </Box>
+    )
+  ]
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
+
+export const Disabled: StoryObj<CheckboxProps> = {
+  args: {
+    disabled: true
+  }
+}
---
+++ packages/react/src/components/checkbox.tsx
@@ -0,0 +1,77 @@
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import { Check } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { keyframes, styled } from '../styles'
+
+const CheckboxContainer = styled(CheckboxPrimitive.Root, {
+  all: 'unset',
+  width: '$6',
+  height: '$6',
+  backgroundColor: '$gray-900',
+  borderRadius: '$xs',
+  lineHeight: 0,
+  cursor: 'pointer',
+  overflow: 'hidden',
+  boxSizing: 'border-box',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: '2px solid $gray-900',
+
+  '&:focus': {
+    borderColor: '$system-300',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  '&[data-state="checked"]': {
+    backgroundColor: '$system-300',
+  },
+})
+
+const slideIn = keyframes({
+  from: {
+    transform: 'translateY(-100%)',
+  },
+  to: {
+    transform: 'translateY(0%)',
+  },
+})
+
+const slideOut = keyframes({
+  from: {
+    transform: 'translateY(0%)',
+  },
+  to: {
+    transform: 'translateY(100%)',
+  },
+})
+
+const CheckboxIndicator = styled(CheckboxPrimitive.Indicator, {
+  color: '$white',
+  width: '$4',
+  height: '$4',
+
+  '&[data-state="checked"]': {
+    animation: `${slideIn} 200ms ease-out`,
+  },
+
+  '&[data-state="unchecked"]': {
+    animation: `${slideOut} 200ms ease-out`,
+  },
+})
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {}
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
