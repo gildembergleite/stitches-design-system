523ad2fcdfe24d8bef9cf79933697cdd5d8d6571
Gildemberg Leite <gildembergleite@gmail.com>
2024-03-31 10:45:34 -0300

    feat: add controls in Text story

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,9 +5,17 @@ import type { Meta, StoryObj } from '@storybook/react'
 export default {
   title: 'Typography/Text',
   component: Text,
-    args: {
+  args: {
     children: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur quas consectetur laudantium sint, cumque reiciendis architecto nesciunt minus unde, itaque sapiente voluptates harum id ea provident animi necessitatibus nemo earum.'
-  }
+  },
+  argTypes: {
+    size: {
+      options: ['xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl', '8xl', '9xl'],
+      control: {
+        type: 'inline-radio'
+      }
+    },
+  },
 } as Meta<TextProps>
 
 export const Primary: StoryObj<TextProps> = {}
